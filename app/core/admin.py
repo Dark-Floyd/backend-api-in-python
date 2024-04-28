c2f423cf73d7d6e0bc60945ad2845a968251abbf
Zohar <zoharzaborski@gmail.com>
2024-04-28 13:51:35 +0300

    done with tags api

--- app/core/admin.py
+++ app/core/admin.py
@@ -46,3 +46,4 @@ class UserAdmin(BaseUserAdmin):
 admin.site.register(models.User, UserAdmin) #without useradmin it wont affect the admin page
 admin.site.register(models.Recipe)
 admin.site.register(models.Tag)
+admin.site.register(models.Ingredient)
